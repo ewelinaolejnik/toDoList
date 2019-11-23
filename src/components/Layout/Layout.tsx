ebeb08dbdeee36acf6b28217504efebd4355ed98
Ewelina <ewelina.olejnik@outlook.com>
2019-11-23 15:06:09 +0100

    Header and style with styled components

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -1,7 +1,10 @@
 import React from 'react';
+import Header from '../Header/Header';
 
 const Layout: React.FunctionComponent<{}> = props => {
-    return null;
+    return <div>
+        <Header />
+    </div>
 };
 
 export default Layout;
