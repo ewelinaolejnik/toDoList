3b4110d86f25e04351725a0ba14df5c06184bcec
Ewelina <ewelina.olejnik@outlook.com>
2019-11-24 15:03:55 +0100

    To Do style

--- src/components/ToDoList/ToDo/ToDo.tsx
+++ src/components/ToDoList/ToDo/ToDo.tsx
@@ -1,11 +1,28 @@
-import React, { FunctionComponent, FunctionComponentElement } from 'react';
+import React, { FunctionComponent } from 'react';
+import styled from 'styled-components';
+
 import { ToDoProps } from '../../../types';
+
+const StyledToDo = styled.div`
+    border: 1px solid #55B5CC;
+    margin-top: 5px;
+    padding:20px;
+    background-color: white;
+    border-radius: 20px;
+    box-shadow: 0px 1px 3px grey;
+`
+
+const ToDoTitle = styled.p`
+    text-align: left;
+`;
+
+
 
 const ToDo: FunctionComponent<ToDoProps> = ({ id, title, completed }) => {
     return (
-        <div>
-            <p>{title}</p>
-        </div>
+        <StyledToDo>
+            <ToDoTitle>{title}</ToDoTitle>
+        </StyledToDo>
     );
 }
 
